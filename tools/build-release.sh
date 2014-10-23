e67f42c41d9c6ffd79f87fda12aaadbbf864b8d3
Andrey Ulanov <aulanov@gmail.com>
2014-10-23 01:09:06 -0700

    Change location of zipalign

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -6,4 +6,4 @@ OUT=altidroid_aligned.apk
 ./gradlew assembleRelease || exit 1
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release.keystore \
   $APK altidroid_release || exit 1
-$ANDROID_HOME/tools/zipalign -f 4 $APK $OUT
+$ANDROID_HOME/build-tools/zipalign -f 4 $APK $OUT
